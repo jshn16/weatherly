5ac731ab87a4f1a81ababc44944edf05f0caf983
jashan <jashannoor2001@gmail.com>
2023-08-26 23:17:24 -0600

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -6,29 +6,25 @@ import PreLoader from "./animations/preLoader";
 import Weather from "./components/weather";
 function App() {
   //useEffect to set title
-  useEffect(() => {
-    document.title = "Weatherly";
-    console.warn(`You sneaky developer \n  what are you doing here?`);
-    function handleContextMenu(event) {
-      event.preventDefault();
-    }
-    window.addEventListener("contextmenu", handleContextMenu);
-  }, []);
+  // useEffect(() => {
+  //   document.title = "Weatherly";
+  //   console.warn(`You sneaky developer \n  what are you doing here?`);
+  //   function handleContextMenu(event) {
+  //     event.preventDefault();
+  //   }
+  //   window.addEventListener("contextmenu", handleContextMenu);
+  // }, []);
 
   return (
     <div className="App">
       <HashRouter>
         <Routes>
           <Route path="/" element={<PreLoader />}></Route>
-          
         </Routes>
-
         <Routes>
           <Route path="home" element={<Weather />}></Route>
-          
         </Routes>
       </HashRouter>
-     
     </div>
   );
 }
